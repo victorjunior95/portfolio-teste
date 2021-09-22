a8b41a1716ce944ed7145d09bd07b38fb9dc6195
victorjunior95 <victorjr.muchacho@gmail.com>
2021-09-22 09:40:05 -0300

    portfolio base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
+import { AboutMe, ContactForm, Projects } from './components';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const NavBar = () => (
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Sobre mim</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projetos</Link>
+        </li>
+        <li>
+          <Link to="/contact">Entre em contato</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/">
+      <AboutMe />
+    </Route>
+
+    <Route path="/contact">
+      <ContactForm />
+    </Route>
+
+    <Route path="/projects">
+      <Projects />
+    </Route>
+
+    <Route>
+      <section>
+        <h1>Página não encontrada</h1>
+      </section>
+    </Route>
+  </Switch>
+);
+
+const App = () => (
+  <div>
+    <NavBar />
+    <Routes />
+  </div>
+);
 
 export default App;
